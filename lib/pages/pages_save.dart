166480f012b90efe0a9e7f95b280def8183090f5
raulBrady <bradyraule@gmail.com>
2020-12-14 12:27:09 -0500

    Formulario Terminado

--- lib/pages/pages_save.dart
+++ lib/pages/pages_save.dart
@@ -5,8 +5,66 @@ class SavePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Guardar"),
+      ),
+      body: Container(
+        child: _FormSave(),
+      ),
+    );
+  }
+}
+
+class _FormSave extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final titleController = TextEditingController();
+  final contenidoController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Text("Listado"),
+      padding: EdgeInsets.all(15),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: titleController,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return "De ingresar datos";
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                  labelText: "Titulo", border: OutlineInputBorder()),
+            ),
+            SizedBox(height: 15),
+            TextFormField(
+              controller: contenidoController,
+              maxLines: 8,
+              maxLength: 1000,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return "De ingresar datos";
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                  labelText: "Contenido", border: OutlineInputBorder()),
+            ),
+            RaisedButton(
+              child: Text("Guardar"),
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  print("Valido");
+                }
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
